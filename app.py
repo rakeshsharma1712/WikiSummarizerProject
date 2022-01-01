5078a626de19024e4067c995b84609f374e06ad9
rakeshsharma1712 <95287626+rakeshsharma1712@users.noreply.github.com>
2022-01-01 17:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 # doing necessary imports
 import threading
-import io
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 from logger_class import getLog
 from flask import Flask, render_template, request, jsonify, Response, url_for, redirect
 from flask_cors import CORS, cross_origin
-import pandas as pd
+
 from mongoDBOperations import MongoDBManagement
 from WikiScrapping import WikiScrapper
 from selenium import webdriver
